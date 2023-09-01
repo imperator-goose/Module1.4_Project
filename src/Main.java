6d6cfec7eff84a9b951be4ecc491bb8fe12853db
imperator-goose <glookeemaska133@gmail.com>
2023-09-01 15:08:48 +0300

    d

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,57 @@
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static class Foo {
-        public static void first(Runnable r) {
-            System.out.print("first"); }
-        public static void second(Runnable r) {
-            System.out.print("second"); }
-        public static void third(Runnable r) {
-            System.out.print("third");
+        public static void first() {
+            System.out.print("first");
         }
+
+        public static void second() {
+            System.out.print("second");
+        }
+
+        public static void third() {
+            System.out.println("third");
+        }
+
         public static void main(String[] args) {
-            NewThread nt1 = new NewThread("A");
-            NewThread nt2 = new NewThread("B");
-            NewThread nt3 = new NewThread("C");
-            first(nt1);
-            second(nt2);
-            third(nt3);
 
+
+            Runnable taskA = () -> {
+                first();
+
+            };
+
+            Runnable taskB = () -> {
+                second();
+            };
+
+            Runnable taskC = () -> {
+                third();
+            };
+    taskA.run();
+    taskB.run();
+    taskC.run();
+    NewThread nt1 = new NewThread("A");
+    NewThread nt2 = new NewThread("B");
+    NewThread nt3 = new NewThread("C");
+    nt1.run();
+    nt2.run();
+    nt3.run();
         }
     }
-static class NewThread implements Runnable{
-            String name;
-            Thread t;
-            NewThread(String threadName){
-                name = threadName;
-                t = new Thread(this, name);
-            }
-            public void run(){
 
-            }
+    static class NewThread implements Runnable {
+        String name;
+
+        NewThread(String threadName) {
+            name = threadName;
+        }
+
+        public void run() {
+            System.out.println("Thread " + name + " is running");
+        }
     }
-
 }
